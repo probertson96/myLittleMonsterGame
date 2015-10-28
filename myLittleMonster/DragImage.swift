e9c648e117ca0838f882c4884e12aa548abd01d6
Patrick Robertson <Patrick@Patricks-MacBook-Pro.local>
2015-10-28 18:39:15 +1100

    Layout completed, animation completed, dragging images completed

---
+++ myLittleMonster/DragImage.swift
@@ -0,0 +1,38 @@
+//
+//  DragImage.swift
+//  myLittleMonster
+//
+//  Created by Patrick Robertson on 28/10/2015.
+//  Copyright © 2015 Patrick Robertson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DragImage: UIImageView {
+    
+    var originalPosition: CGPoint!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        originalPosition = self.center
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            let position = touch.locationInView(self.superview)
+            self.center = CGPointMake(position.x, position.y)
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+    }
+}
--- myLittleMonster/ViewController.swift
+++ myLittleMonster/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var monsterImage: UIImageView!
+    
+    @IBOutlet weak var heartImage: DragImage!
+    @IBOutlet weak var foodImage: DragImage!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Animations
+        var imageArray = [UIImage]()
+        for var x = 1; x <= 4; x++ {
+            let image = UIImage(named: "idle\(x).png")
+            imageArray.append(image!)
+        }
+        
+        monsterImage.animationImages = imageArray
+        monsterImage.animationDuration = 0.8
+        monsterImage.animationRepeatCount = 0
+        monsterImage.startAnimating()
     }
+    
 
 }
 
